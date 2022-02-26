e8eea2dd25e95783bb253fb431bf28173165ea1a
Adrian Rangel <adrijar21@gmail.com>
2022-02-26 00:04:24 -0400

    add one loader

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+// LISTENER OF THE LOAD EVENT
 window.addEventListener('click', () => {
 
+    // CONSTANTS 
     const advicee = document.querySelector('.advice');
     const p = document.querySelector('.quote');
     const dice = document.querySelector('.dice--img');
+    const loader = document.querySelector('.loader');
     const url = 'https://api.adviceslip.com/advice';
 
-    
-    dice.addEventListener('click', () => {
-
+    // FUNCTION OF THE FETCH AND THE MANAGE OF THE DATA
+    const apiFetch = ()=> { 
         
-        fetch(url)
+        fetch(url, {'cache': "no-cache"}) // THIS IS USEFULL ?¿
             .then(response => { return response.json() })
             .then(data => {
 
@@ -18,10 +20,26 @@ window.addEventListener('click', () => {
 
                 p.innerHTML = data.slip.advice;
 
-            })
+            }).catch(e => {
+                alert(e)
+            });  
+        }
+
+    // FUNCTION OF THE LOADING ELEMENT
+    const loading = () => {
+        loader.classList.add('loading');
+    }
 
 
+    // LISTENER OF THE CLICK EVENT
+    dice.addEventListener('click', () => {
 
+        setTimeout(() => {
+            loader.classList.remove('loading');
+            apiFetch();
+        }, 2500)
+        loading();
+        
     });
 
     
